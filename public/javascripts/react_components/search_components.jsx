a1d92ae536f73f33d77278ce7f933e8c283a9a2f
cchens <mail@christinachen.ca>
2016-11-29 13:49:29 -0500

    changed search to be form

--- public/javascripts/react_components/search_components.jsx
+++ public/javascripts/react_components/search_components.jsx
@@ -37,27 +37,21 @@ var SearchPanel = React.createClass({
 			<div className="container">
         <div id="search_column" className="col-md-12 text-center">
         	<Panel id="advanced_search" title="Advanced Search">
-        		<div className="col-md-6 text-left">
-        			<div className="input-group">
-	        			<p>First Name</p>
-	        			<input type="text" className="form-control" placeholder="John" />
+	        	<form id="register_form" action="/register" method="post">
+	        		<div className="col-md-6 text-left">
+	        			<p>First Name</p> <input type="text" className="form-control" placeholder="John" />
+	        			<p>Student Number</p> <input type="text" className="form-control" placeholder="123456789" />
 	        		</div>
-	        		<div className="input-group">
-	        			<p>Student Number</p>
-	        			<input type="text" className="form-control" placeholder="123456789" />
+	        		<div className="col-md-6 text-center">
+	        			<p>Last Name</p> <input type="text" className="form-control" placeholder="Smith" />
+	        			<p>Email</p> <input type="text" className="form-control" placeholder="example@example.com" />
 	        		</div>
-        		</div>
-        		<div className="col-md-6 text-center">
-        			<div className="input-group">
-	        			<p>Last Name</p>
-	        			<input type="text" className="form-control" placeholder="Smith" />
-	        		</div>
-	        		<div className="input-group">
-	        			<p>Email</p>
-	        			<input type="text" className="form-control" placeholder="example@example.com" />
-	        		</div>
-        		</div>
-        		<button type="button" className="btn btn-default" onClick={this.props.onClick.bind(null, this)}>Search!</button>
+	        		<input id="register_button" 
+                     className="btn btn-default center-block" 
+                     type="submit" 
+                     onClick={this.props.onClick.bind(null, this)}}
+                     value="Register" />
+        		</form>
         	</Panel>
         </div>
       </div>
