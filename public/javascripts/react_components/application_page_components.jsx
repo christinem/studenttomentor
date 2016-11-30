370a0e1b8eb31100cfb825e0cf1179ac442e424a
Christine Murad <christine.murad@mail.utoronto.ca>
2016-11-30 01:19:15 -0500

    Pushed work for date picker

--- public/javascripts/react_components/application_page_components.jsx
+++ public/javascripts/react_components/application_page_components.jsx
@@ -14,32 +14,29 @@ var ApplicationPage = React.createClass({
           <h1 className="title"> Application Page </h1>
         </div>
 
-        <div className="col-md-4 text-center">
-
-        </div>
-        <div className="col-md-8 text-center">
+        <div className="col-md-12 text-center">
           <Panel id="application_info" title="Application Information">
             <div className="row">
-              <div id="application_information" className="col-md-6 text-center">
+              <div id="application_information" className="col-md-12 text-center">
                 <Panel title="Expected Graduation Date">
-                  <div className="dropdown">
-                    <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                      Month
-                    </button>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-                      <li><a href="#">January</a></li>
-                      <li><a href="#">February</a></li>
-                      <li><a href="#">March</a></li>
-                      <li role="separator" className="divider"></li>
-                    </ul>
-                  </div>
-                  <div className="input-group">
-                    <input type="text" className="form-control" placeholder="Year" aria-describedby="basic-addon1">
-                    </input>
-                  </div>
+                  {/* Date picker reference: https://eonasdan.github.io/bootstrap-datetimepicker/*/}
+                  <div className="container">
+                    <div className="row">
+                        <div className='col-sm-6'>
+                            <div className="form-group">
+                                <div className='input-group date' id='datetimepicker1'>
+                                    <input type='text' className="form-control" />
+                                    <span className="input-group-addon">
+                                        <span className="glyphicon glyphicon-calendar"></span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 </Panel> 
               </div>
-              <div className="col-md-6 text-center">
+              <div className="col-md-12 text-center">
                 <Panel id="past_participation" title="Have you been part of the mentorship program in the past?"> </Panel>
               </div>
             </div>
