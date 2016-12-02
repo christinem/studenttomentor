48f22e62ead2a4ea41c34d7e3268fb71a3463172
Christine Murad <christine.murad@mail.utoronto.ca>
2016-12-02 15:31:48 -0500

    Now includes a view button and keys are decreased

--- public/javascripts/react_components/view_all_page_components.jsx
+++ public/javascripts/react_components/view_all_page_components.jsx
@@ -3,6 +3,34 @@ import {render} from "react-dom";
 import {NavBar, Panel} from "./common_components.jsx";
 
 var ViewAllPage = React.createClass({
+  getInitialState: function() {
+    return {
+      data: []
+    }
+  },
+
+  componentDidMount: function() {
+    var new_data = []
+    data.map(function(obj) {
+      var new_object = {}
+      Object.keys(obj).map(function(key) {
+        if (key == "first_name") {
+          new_object["First Name"] = obj[key];
+        } else if (key == "last_name") {
+          new_object["Last Name"] = obj[key];
+        } else if (key == "email") {
+          new_object["Email"] = obj[key];
+        } else if (key == "expected_grad") {
+           new_object["Application #"] = obj["id"];
+        }
+      });
+
+      new_data.push(new_object);
+    });
+    
+    this.setState({data: new_data});
+  },
+
   render: function() {
     return (
       <div>
@@ -11,12 +39,15 @@ var ViewAllPage = React.createClass({
         <div className="container">
           <div id="search_column" className="col-md-12 text-left">
             <Panel id="view_all" title="View All">
-              {data.map(function(data_item) {
+              {this.state.data.map(function(data_item) {
                 return (
                   <ul className="list-group">
                     {Object.keys(data_item).map(function(key) {
                         return <li className="list-group-item">{key}: {data_item[key]}</li>;
                     })}
+                    <li className="list-group-item">
+                      <a className="btn btn-default" role="button">View </a>
+                    </li>
                   </ul>
                 )
               })}
