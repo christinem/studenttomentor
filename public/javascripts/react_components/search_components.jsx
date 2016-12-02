06f5f2a8c0faae3c457ac3d26d265ca3c0075462
cchens <mail@christinachen.ca>
2016-12-01 20:50:23 -0500

    draft of search

--- public/javascripts/react_components/search_components.jsx
+++ public/javascripts/react_components/search_components.jsx
@@ -1,70 +1,110 @@
 import React from "react";
-import { render } from "react-dom";
+import {render} from "react-dom";
 import {NavBar, Panel} from "./common_components.jsx";
 
-var SearchPage = React.createClass ({
+var SearchPage = React.createClass({
 
 	getInitialState: function() {
 		return {
-			listUsers: false
+			submitted: false,
+			users: "",
+			filtered: ""
 		}
 	},
 
-	onClick: function() {
-		this.setState({listUsers: true});
-	},
+	onSubmit: function(event) {
+		event.preventDefault();
 
-	render: function() {
+    var formData = {
+      first_name: this.refs.fname.value,
+      last_name: this.refs.lname.value,
+      student_number: this.refs.stunum.value,
+      email: this.refs.email.value
+    };
 
-		if (this.state.listUsers) {
-			var pageContents = <UserList />
-		} else {
-			var pageContents = <SearchPanel onClick={this.onClick} />
-		}
-
-		return (
-			<div>
-				<NavBar />
-				{pageContents}
-			</div>
+    $.get("/users/students", function (data) {
+        this.setState({submitted: true, users: data}, function() {
+        	this.filterUsers(formData);
+        });
+        document.getElementById("user_panel").scrollIntoView();
+    	}.bind(this)
 		);
-	}
-});
+	},
 
-var SearchPanel = React.createClass({
+	filterUsers: function(formData) {
+
+		let users = this.state.users;
+
+		for (var key in formData) {
+	    if (formData[key]) {
+	    	users = users.filter(function (user) {
+	    		return user[key] == formData[key];
+	    	});
+	    }
+		}
+
+		this.setState({users: users});
+	},
+
 	render: function() {
+
+		var filtered = [];
+
+		for (var user of this.state.users) {
+			var arr = [];
+			arr.push(<div className="firstName">First Name: {user.first_name}</div>);
+			arr.push(<div className="lastName">Last Name: {user.first_name}</div>);
+			arr.push(<div className="studentNumer">Student Number: {user.student_number}</div>);
+			arr.push(<div className="email">Email: {user.email}</div>);
+			arr.push(<div className="email">Interests: {user.interests}</div>);
+			arr.push(<div className="about">About: {user.about_text}</div>);
+			arr.push(<br />);
+			filtered.push(arr);
+		}
+
+		if (this.state.submitted) {
+    	var userList = <Panel id="user_list" title="Results">
+    								   {filtered}
+                     </Panel>;
+    }
+
 		return (
 			<div className="container">
         <div id="search_column" className="col-md-12 text-center">
         	<Panel id="advanced_search" title="Advanced Search">
-	        	<form id="register_form" action="/register" method="post">
+	        	<form id="search_form" action="" onSubmit={this.onSubmit}>
 	        		<div className="col-md-6 text-left">
-	        			<p>First Name</p> <input type="text" className="form-control" placeholder="John" />
-	        			<p>Student Number</p> <input type="text" className="form-control" placeholder="123456789" />
-	        		</div>
+			          <div className="form-group">
+			            <label htmlFor="fname">First Name</label>
+			            <input className="form-control" name="fname" ref="fname" placeholder="John" pattern="[A-Za-z]+" />
+			          </div>
+			          <div className="form-group">
+			            <label htmlFor="stunum">Student Number</label>
+			            <input className="form-control" name="stunum" ref="stunum" placeholder="123456789" pattern="\d{9}(\d{1})?" />
+			          </div>
+			        </div>
 	        		<div className="col-md-6 text-center">
-	        			<p>Last Name</p> <input type="text" className="form-control" placeholder="Smith" />
-	        			<p>Email</p> <input type="text" className="form-control" placeholder="example@example.com" />
+	        			<div className="form-group">
+			            <label htmlFor="lname">Last Name</label>
+			            <input className="form-control" name="lname" ref="lname" placeholder="Smith" pattern="[A-Za-z]+" />
+			          </div>
+			          <div className="form-group">
+			            <label htmlFor="email">Email</label>
+			            <input className="form-control" name="email" ref="email" placeholder="example@example.com" pattern="\w+@[A-Za-z]+\.[A-Za-z]+"/>
+			          </div>
 	        		</div>
 	        		<input id="register_button" 
                      className="btn btn-default center-block" 
                      type="submit" 
-                     onClick={this.props.onClick.bind(null, this)}
                      value="Register" />
         		</form>
         	</Panel>
+        	<div id="user_panel">
+        		{userList}
+        	</div>
         </div>
       </div>
     );
-	}
-});
-
-var UserList = React.createClass({
-	render: function() {
-		return (
-			<div>
-			</div>
-		);
 	}
 });
 
