d2c75ff272a08f99611529c7f4be1e8c17fe80fe
cchens <mail@christinachen.ca>
2016-12-04 21:15:58 -0500

    undo sequelize

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('postgres://bmoskofzslpxbv:03busDyXJ64fS5qW9023aHuhws@ec2-54-227-237-223.compute-1.amazonaws.com:5432/d8g4lbslj0r1j9');
-var sequelize = new Sequelize('postgres://localhost:5432/studenttomentor');
+//var sequelize = new Sequelize('postgres://localhost:5432/studenttomentor');
 
 module.exports = sequelize;
